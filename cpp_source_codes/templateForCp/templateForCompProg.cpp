adfe7694b2831d14a926280f2511d239e150f03a
karan51ngh <karansingh9535@gmail.com>
2021-07-01 17:10:02 +0530

    added a template for competitive programming

---
+++ cpp_source_codes/templateForCp/templateForCompProg.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define int long
+#define deb(x) cout << #x << " is " << x << endl
+using namespace std;
+
+void solved();
+
+signed main()
+{
+	int T;
+	cin >> T;
+	while (T--)
+		solved();
+}
+void solved()
+{
+	int i = 9;
+	deb(i);
+}
